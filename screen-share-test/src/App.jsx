9c936eee220eeb5205ad13c2d24cd5f105c7b3bb
SumantSingh08 <sumant082003@gmail.com>
2026-02-22 22:19:05 +0530

    first - commit

--- screen-share-test/src/App.jsx
+++ screen-share-test/src/App.jsx
@@ -1,15 +1,15 @@
-import { Routes, Route, HashRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import ScreenTest from "./pages/ScreenTest";
 import "./App.css";
 function App() {
   return (
-    <HashRouter>
+    
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/screen-test" element={<ScreenTest />} />
       </Routes>
-    </HashRouter>
+
   );
 }
 
